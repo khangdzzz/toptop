879f6582e48802b820474678768fc5bd5226fb71
Do Van Khang <do.van.khang@azoom.jp>
2023-04-23 17:02:56 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useReducer, useRef } from 'react'
+
+
+const initial = {
+  job: '',
+  jobs: []
+}
+
+const Add = 'add'
+const Delete = 'delete'
+const Set = 'set'
+
+
+
+const reducer = (state, action) => {
+  let newState
+
+  switch (action.type) {
+    case Set:
+      newState = {
+        ...state,
+        job: action.payload
+      }
+      break
+    case Add:
+      newState = {
+        ...state,
+        job: '',
+        jobs: [...state.jobs, action.payload]
+      }
+      break
+    case Delete: {
+      state.jobs.splice(action.payload, 1)
+      newState = {
+        ...state
+      }
+      break
+    }
+    default:
+      throw new Error('Invalid action !')
+  }
+  return newState
+}
+
+function logger(reducerFun) {
+
+  return (preState, action) => {
+    const newState = reducerFun(preState, action)
+
+    return newState
+  }
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(logger(reducer), initial)
+  const inputRef = useRef()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '50px' }}>
+      <input
+        ref={inputRef}
+        value={state.job}
+        placeholder="Enter the job...."
+        onChange={(e) => {
+          dispatch({
+            type: Set,
+            payload: e.target.value
+          })
+        }}
+      />
+      <button onClick={() => {
+        inputRef.current.focus()
+        dispatch({
+          type: Add,
+          payload: state.job
+        })
+      }
+      }
+      >add</button>
+
+      <ul>
+        {state.jobs.map((job, index) => (
+          <li
+            key={index}
+          >
+            {job}
+            <span
+              style={{ marginLeft: '20px', cursor: 'pointer' }}
+              onClick={() =>
+                dispatch({
+                  type: Delete,
+                  payload: index
+                })
+              }
+            >
+              x
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
